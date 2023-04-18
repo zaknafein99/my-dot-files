2859cec6ddd205d09ca91967ca6513580bb66e36
Ismael Marquez <imarquez99@gmail.com>
2023-04-17 23:26:08 -0300

    Add zsh zinit_plugins

---
+++ .zsh/zinit_plugins.zsh
@@ -0,0 +1,12 @@
+# Plugin history-search-multi-word loaded with investigating.
+zinit load zdharma-continuum/history-search-multi-word
+
+#SDKMan
+zinit load matthieusb/zsh-sdkman
+
+# Two regular plugins loaded without investigating.
+zinit light zsh-users/zsh-autosuggestions
+zinit light zdharma-continuum/fast-syntax-highlighting
+
+# Snippet
+zinit snippet https://gist.githubusercontent.com/hightemp/5071909/raw/
