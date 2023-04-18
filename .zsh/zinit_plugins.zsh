1b02fe70026a220015be44d0311e688f7ad281c7
Ismael Marquez <imarquez99@gmail.com>
2023-04-17 23:32:23 -0300

    Add sudo zsh plugin

--- .zsh/zinit_plugins.zsh
+++ .zsh/zinit_plugins.zsh
@@ -10,3 +10,7 @@ zinit light zdharma-continuum/fast-syntax-highlighting
 
 # Snippet
 zinit snippet https://gist.githubusercontent.com/hightemp/5071909/raw/
+
+#Oh-My-Zsh plugins
+
+zinit snippet 'https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/sudo/sudo.plugin.zsh'
