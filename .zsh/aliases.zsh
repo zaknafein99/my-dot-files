715a24c001ee7309695f23d2c075aa6e5c8fa734
Ismael Marquez <imarquez99@gmail.com>
2023-04-17 22:19:15 -0300

    Update zsh config

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,27 +1,11 @@
 # Alias
-# ---
-#
-alias k="kubectl"
-alias h="helm"
-alias tf="terraform"
-alias a="ansible"
-alias ap="ansible-playbook"
-alias dt="datree"
-
-# mac OS shortcuts
-alias code="open -a 'Visual Studio Code'"
-
-
 
 # ALIAS COMMANDS
 alias ls="exa --icons --group-directories-first"
 alias ll="exa --icons --group-directories-first -l"
 alias la="exa --icons --group-directories-first -la"
-alias g="goto"
 alias grep='grep --color'
+alias vim="nvim"
 
 alias cat=bat
-alias emacs="emacs-28.1"
-alias emacsc="emacsclient -c -a 'emacs'"
-alias cbp="code /home/xcad/obsidianvault/boilerplates"
-alias cpr="code /home/xcad/obsidianvault/projects"
+
