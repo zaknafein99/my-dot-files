5f81a669488e59d07b7099cc1a02b83ae3f0879d
Ismael Marquez <imarquez99@gmail.com>
2023-04-24 00:09:05 -0300

    Some configuration changes en added Fish config

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -6,7 +6,8 @@ alias ll="exa --icons --group-directories-first -l"
 alias la="exa --icons --group-directories-first -la"
 alias grep='grep --color'
 alias vim="nvim"
-alias ec="emacsclient -c"
+alias ec="emacsclient -c -a 'emacs'"
+alias emacs="emacsclient -c -a 'emacs'"
 
 alias cat=bat
 
