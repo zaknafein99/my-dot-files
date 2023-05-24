f4fed13d6e0e36112496655bef336946495b97df
Ismael Marquez <imarquez99@gmail.com>
2023-05-24 18:42:38 -0300

    Add aliases and alacritty config

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -5,8 +5,16 @@ alias ls="exa --icons --group-directories-first"
 alias ll="exa --icons --group-directories-first -l"
 alias la="exa --icons --group-directories-first -la"
 alias grep='grep --color'
+<<<<<<< Updated upstream
 alias vim="nvim"
 alias ec="emacsclient -c"
+=======
+alias vim="lvim"
+alias ec="emacsclient -c -a 'emacs'"
+alias emacs="emacsclient -c -a 'emacs'"
+alias top="bpytop"
+alias wezterm='flatpak run org.wezfurlong.wezterm'
+>>>>>>> Stashed changes
 
 alias cat=bat
 
